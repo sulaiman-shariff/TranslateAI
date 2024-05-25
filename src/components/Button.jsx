4a7e126b8e3d98531667a939c3ac1a575d054b40
sulaiman-shariff <sulaiman05221@gmail.com>
2024-05-26 03:32:18 +0530

    Adding button for AI and updating logo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Button = ({ styles }) => (
   <div>
-    
     <a href="src/convert.html" target="_blank">
     <button type="button" className={`ml-4 py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-      Try now!
+      Try our migration tools now!
+    </button>
+    </a>
+    <a href="src/chatbot.html" target="_blank">
+    <button type="button" className={`ml-4 py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+      Try the chatbot!
     </button>
     </a>
  </div>
